c57f1b83f4a249dc7dcd740d5c8c1557464fac14
ilyjyd <ilyassjayed01@gmail.com>
2022-06-03 15:20:49 +0100

    -3

---
+++ app/src/main/java/com/example/ilias_jayed/MyDatabase.java
@@ -0,0 +1,17 @@
+package com.example.ilias_jayed;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MyDatabase extends SQLiteOpenHelper {
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
